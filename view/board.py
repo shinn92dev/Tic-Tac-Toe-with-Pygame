1905cecc7a25bbb016079a653ac7d3e46e719223
Anthony Shin <shinn92dev@gmail.com>
2024-01-02 15:46:49 -0800

    implement: draw main board to the window

--- view/board.py
+++ view/board.py
@@ -5,11 +5,29 @@ from random import randint
 WINDOW_SIZE = 600
 
 
+class TictactoeTool:
+    def __init__(self, game):
+        self.game = game
+        self.board_image = self.get_image(path="../images/board.png", res=[WINDOW_SIZE] * 2)
+
+    def draw(self):
+        self.game.screen.blit(self.board_image, (0, 0))
+
+    @staticmethod
+    def get_image(path, res):
+        img = pg.image.load(path)
+        return pg.transform.smoothscale(img, res)
+
+    def run(self):
+        self.draw()
+
+
 class TictactoeWindow:
     def __init__(self):
         pg.init()
         self.screen = pg.display.set_mode([WINDOW_SIZE] * 2)
         self.clock = pg.time.Clock()
+        self.tic_tac_toe = TictactoeTool(self)
 
     def check_events(self):
         for event in pg.event.get():
@@ -19,6 +37,7 @@ class TictactoeWindow:
 
     def run(self):
         while True:
+            self.tic_tac_toe.run()
             self.check_events()
             pg.display.update()
             self.clock.tick(60)
